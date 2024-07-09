76e4edaab5e419a0c8f74d93c00c3cbfdf101cd7
elzup <guild0105@gmail.com>
2024-07-10 00:40:30 +0900

    add os_command_injeciton demo

---
+++ os_command_injeciton/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/list_users", methods=["GET"])
+def list_users():
+    target = request.args.get("target", "")
+    command = f"ls {target}"
+    result = os.popen(command).read()
+    return f"<pre>{result}</pre>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
